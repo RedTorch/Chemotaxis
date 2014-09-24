87d13aa92c7e161427dd65521a8f8e391f1a7dbb
RedTorch <kenta.medina@gmail.com>
2014-09-24 10:38:05 -0700

    Added first name to index.html

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] colony;
 int playX = 300;
 int playY = 300;
-int playHP = 10;
+int playHP = 100;
 int wepRange = 100;
 int ammo = 200;
 int bactRange = 130;
@@ -9,7 +25,7 @@ int playKill = 0;
 int bacNum = 400;
 boolean released = true;
 boolean gameover = false;
- void setup()   
+ public void setup()   
  {     
    frameRate(30);
    size(600,600);
@@ -20,7 +36,7 @@ boolean gameover = false;
     }
     noCursor();
  }   
- void draw()   
+ public void draw()   
  { 
    background(0);
    playMove();
@@ -96,10 +112,10 @@ boolean gameover = false;
      myY = y;
      clr = 250;
    }
-   void move()
+   public void move()
    {
-     myX = myX + (int)(Math.random()*5-2.5);
-     myY = myY + (int)(Math.random()*5-2.5);
+     myX = myX + (int)(Math.random()*5-2.5f);
+     myY = myY + (int)(Math.random()*5-2.5f);
      if(dist(myX, myY, playX, playY)<bactRange)
      {
        if(myX < playX)
@@ -125,14 +141,14 @@ boolean gameover = false;
        }
      }
    }
-   void show()
+   public void show()
    {
      noFill();
      stroke(0,150,clr);
         rect(myX-5,myY-5,10,10);
    }
  }
- void playMove()
+ public void playMove()
  {
    if(playX < mouseX)
      playX = playX + 2;
@@ -143,7 +159,7 @@ boolean gameover = false;
    else if(playY > mouseY)
      playY = playY - 2;
  }    
- void firing()
+ public void firing()
  {
      int minDist = 600;
      int minBac = 0;
@@ -165,7 +181,16 @@ boolean gameover = false;
        released = false;
      }
  }
- void mouseReleased()
+ public void mouseReleased()
  {
    released = true;
  }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
